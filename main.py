1a95213b76b7f7c631251d31a17e6a1e8890c0c5
msc128 <105659799+msc-128@users.noreply.github.com>
2022-05-19 15:08:17 +0000

    combined code

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
-import random
 from Titles import printTitle
+from Chatbotwiki import wiki
 print("Welcome to")
 printTitle()
 print("------------------------\nMenu:\n1 - Wikipedia Lookup\n2 - Favorite Sites\n3 - Stop")
-firstInput = input("> ")
+while True:
+  firstInput = input("> ")
+  if firstInput == "1":
+    wiki()
+    continue
+  elif firstInput == "2":
+    f = open("madLibFile.txt", "r")
+    print(f.read())
+    print("Do you want to edit your favorite sites?")
+    secInput = input("> ")
+    if secInput == "yes":
+      print("Type the sites you want to add")
+      favInput = input("> ")
+    else:
+      favInput = ""
+      print("Done")
+    try:
+      madFile=open('madLibFile.txt', mode='r', encoding='utf-8')
+    except:
+      print("Problem opening madLibFile.txt.")
+    madFile.close()
+    f = open("madLibFile.txt", "a")
+    f.write(favInput + "\n")
+    f.close()
+    f = open("madLibFile.txt", "r")
+    print(f.read())
 
-
-def wikiSearch():
-    print("What would you like to search for?")
-    wikiInput = input("> ")
-
-def favSites():
-  print("")
+  elif firstInput == "3":
+    break
+  else:
+    continue
