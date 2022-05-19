baea1afb703315edf26f616f43fa52081d406bf8
APT0128 <105660104+APT0128@users.noreply.github.com>
2022-05-19 09:03:10 -0700

    Add files via upload

---
+++ Chatbotwiki.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+base_url = "https://en.wikipedia.org/wiki/"
+while True:
+    topic = input("Search a topic on wikipedia (Use '_' for spacing and 'exit' to exit): ")
+    if topic == 'exit':
+        break
+    ending = topic
+    url = base_url + ending
+    r = requests.get(url)
+    print("URL: ",url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    tags = soup.find_all('p')
+    topic2 = topic.replace("_", " ")
+    for tag in tags:
+        if topic2 in tag.text:
+            #print(tag.text)
+            break
+    txt = tag.text
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    roman = ['I','II','III','IV','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+    for x in alphabet:
+        target = '['+x+']'
+        txt = txt.replace(target,'')
+    for y in roman:
+        target2 = '['+y+']'
+        txt = txt.replace(target2,'')
+    for i in range(50):
+        target1 = '['+str(i)+']'
+        txt = txt.replace(target1,'')
+    txt = txt.replace('(listen)','')
+    txt = txt.replace('[update]','')
+    print(txt)
+    
+
+
